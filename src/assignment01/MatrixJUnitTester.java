387b544923097651d4077371cb17552980e5b180
Anirudh Lath <anirudhlath@gmail.com>
2021-11-09 00:41:45 -0700

    Added assignment01 and Matrix.equals() is fully functional.

---
+++ src/assignment01/MatrixJUnitTester.java
@@ -0,0 +1,47 @@
+/*
+ * Here is a starting point for your Matrix tester. You will have to fill in the rest with
+ * more code to sufficiently test your Matrix class. We will be using our own MatrixTester for grading. 
+*/
+package assignment01;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatrixJUnitTester {
+
+  Matrix threeByTwo, twoByThree, twoByTwoResult;
+  private Matrix threeByTwoDuplicate;
+  /* Initialize some matrices we can play with for every test! */
+
+  @Before
+  public void setup() {
+    threeByTwo = new Matrix(new int[][] { { 1, 2, 3 }, { 2, 5, 6 } });
+    threeByTwoDuplicate = new Matrix(new int[][] { { 1, 2, 3 }, { 2, 5, 6 } });
+    twoByThree = new Matrix(new int[][] { { 4, 5 }, { 3, 2 }, { 1, 1 } });
+    // this is the known correct result of multiplying M1 by M2
+    twoByTwoResult = new Matrix(new int[][] { { 13, 12 }, { 29, 26 } });
+  }
+
+  @Test
+  public void timesWithBalancedDimensions() {
+    Matrix matrixProduct = threeByTwo.times(twoByThree);
+    Assert.assertTrue(twoByTwoResult.equals(matrixProduct));
+  }
+
+  @Test
+  public void timesWithUnbalancedDimensions() {
+    // TODO: More tests like these!
+  }
+
+  @Test
+  public void twoByTwoToString() {
+    String resultString = "what should it equal?";
+    Assert.assertEquals(resultString, twoByTwoResult.toString());
+  }
+
+  @Test
+  public void equals() {
+    Assert.assertTrue(threeByTwo.equals(threeByTwoDuplicate));
+  }
+}
