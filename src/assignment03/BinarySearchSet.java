babb61fa4bd45cdd14a575406f88d6faf5ad4572
Anirudh Lath <anirudhlath@gmail.com>
2021-11-22 02:19:46 -0700

    Almost finished assignment 3

--- src/assignment03/BinarySearchSet.java
+++ src/assignment03/BinarySearchSet.java
@@ -2,14 +2,22 @@ package assignment03;
 
 import java.util.*;
 
+/**
+ * The class that can create, search and edits sets based on binary search algorithm.
+ *
+ * @param <E> the generic type parameter
+ */
 public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
 
     private int capacity;
-    public int size = 0;
-    public E[] data;
+    private int size = 0;
+    private E[] data;
     private Comparator myComparator;
-    boolean hasNaturalComparator;
+    private boolean hasNaturalComparator;
 
+    /**
+     * Instantiates a new Binary search set.
+     */
     public BinarySearchSet() {
         // Initialise member variables
         capacity = 40;
@@ -18,6 +26,11 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
         hasNaturalComparator = true;
     }
 
+    /**
+     * Instantiates a new Binary search set.
+     *
+     * @param comparator the comparator
+     */
     public BinarySearchSet(Comparator<? super E> comparator) {
         // Initialise member variables
         capacity = 40;
@@ -26,26 +39,33 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
         hasNaturalComparator = false;
     }
 
+    /**
+     * Driver function that checks if the set elements can be compared and if they can be then call binarySearch()
+     *
+     * @param element the element
+     * @throws ClassCastException if the elements cannot be compared naturally.
+     * @return the index of the search element or the element that is lower than the element that is supposed to be added.
+     */
     public int driver(E element) {
         if(hasNaturalComparator) {
             try {
-                Comparator comparator = Comparator.naturalOrder();
-                comparator.compare(element, element);
+                myComparator.compare(element, element); // Check if the elements can be compared using a natural comparator.
             } catch (ClassCastException e) {
-                System.err.println(e.getMessage());
-                System.out.println("The object type is not comparable! Please assign a comparable!");
-                System.exit(-1);
+                throw new ClassCastException("The object type is not comparable. Please assign a custom comparator."); 
             }
         }
-        //System.out.println(size);
         return binarySearch(data, element, 0, this.size - 1);
     }
 
-    public int getSize() {
-        return size;
-    }
-
-
+    /**
+     * Binary search that returns an index.
+     *
+     * @param arr     the array of elements
+     * @param element the element that is being searched for
+     * @param low     the lower index bound
+     * @param high    the higher index bound
+     * @return the index
+     */
     public int binarySearch(E[] arr, E element, int low, int high) {
 //        System.out.println("Size: " + size);
 //        System.out.println("Low: " + low + " | High: " + high);
@@ -53,31 +73,62 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
             int mid = low + (high - low) / 2;
             //System.out.println(mid);
 
-            if (myComparator.compare(arr[mid], element) == 0) {
-                return mid;
-            }
+            try {
+                if (myComparator.compare(arr[mid], element) == 0) {
+                    return mid;
+                }
 
-            if (myComparator.compare(arr[mid], element) > 0) {
-                return binarySearch(arr, element, low, mid - 1);
-            }
+                if (myComparator.compare(arr[mid], element) > 0) {
+                    return binarySearch(arr, element, low, mid - 1);
+                }
 
-            if (myComparator.compare(arr[mid], element) < 0) {
-                return binarySearch(arr, element, mid + 1, high);
+                if (myComparator.compare(arr[mid], element) < 0) {
+                    return binarySearch(arr, element, mid + 1, high);
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Size: " + size);
+                System.out.println("Low: " + low);
+                System.out.println("High: " + high);
+                System.out.println("Mid: " + mid);
+                System.out.println("arr[mid]: " + arr[mid]);
+                System.out.println("arr[size - 1]: " + arr[size - 1]);
+                System.out.println("element: " + element);
+                System.out.println("array: " + Arrays.toString(data));
+
+
+                e.printStackTrace();
             }
         }
-        if (high < 0) {
-//            System.out.println("Case 1");
+        if (size != 0 && myComparator.compare(element, arr[0]) < 0) {
+            return 0;
+        } else if (size != 0 && myComparator.compare(element, arr[size - 1]) > 0) {
+            return size;
+        } else if(size != 0) {
+            if (myComparator.compare(element, arr[low]) > 0) {
+                return low + 1;
+            } else {
+                return low;
+            }
+            /*if (low >= high) {
+                return low;
+            } else {
+                return low + 1;
+            }*/
+        }
+        return 0;
+        /*if (high < 0) {
+            System.out.println("Case 1");
             return 0;
         } else if(low == size) {
-//            System.out.println("Case 2");
+            System.out.println("Case 2");
             return low;
         } else if(low >= high) {
-//            System.out.println("Case 3");
+            System.out.println("Case 3");
             return high + 1;
         } else {
             System.exit(-1);
             return -1;
-        }
+        }*/
     }
 
 
@@ -113,7 +164,7 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
     @Override
     public E last() throws NoSuchElementException {
         if (size != 0) {
-            return data[data.length - 1];
+            return data[size - 1];
         }
         throw new NoSuchElementException("The array is empty.");
     }
@@ -122,20 +173,20 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
      * Adds the specified element to this set if it is not already present and
      * not set to null.
      *
-     * @param element@return true if this set did not already contain the specified element
+     * @param element element to be added
+     * @return true if this set did not already contain the specified element
      */
     @Override
     public boolean add(E element) {
-        int index = this.driver(element);
-        if (index < size && index >= 0) {
+        int index = this.driver(element); // Call the driver
+        /*if (index < size && index >= 0) { // Case 1
             if (data[index] != element) {
                 if (size < capacity - 5) {
                     this.size++;
-                    for (int i = size + 1; i >= index; i--) {
+                    for (int i = size + 1; i >= index; i--) { // Shift the elements to make space for the element to be added.
                         data[i + 1] = data[i];
                     }
-                    data[index] = element;
-                    System.out.println("Added: " + Arrays.toString(data));
+                    data[index] = element; // Add element to the anticipated location if it did not already exist.
                     return true;
                 } else {
                     this.size++;
@@ -149,15 +200,14 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
                     for (int i = index + 1; i < size; i++) {
                         data[i] = temp[i];
                     }
-                    System.out.println("Added: " + Arrays.toString(data));
                     return true;
                 }
             }
-        } else if (index == size) {
+
+        } else if (index == size) { // Case 2
             if (size < capacity - 5) {
                 this.size++;
                 data[index] = element;
-                System.out.println("Added: " + Arrays.toString(data));
                 return true;
             } else {
                 this.size++;
@@ -168,11 +218,35 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
                     data[i] = temp[i];
                 }
                 data[index] = element;
-                System.out.println("Added: " + Arrays.toString(data));
                 return true;
             }
         }
-        System.out.println("Not Added: " + Arrays.toString(data));
+        */
+        if (size >= (capacity - 5)) {
+            capacity = capacity * 2;
+            E[] temp = data;
+            System.out.println("Increasing capacity");
+            data = (E[]) new Object[capacity];
+            for (int i = 0; i < size; i++) {
+                data[i] = temp[i];
+            }
+        } else if (data[index] != element){
+            if (index == size) {
+                System.out.println("Case 1");
+                data[index] = element;
+                size++;
+                return true;
+
+            } else {
+                System.out.println("Case 2");
+                CustomIterator iterator = new CustomIterator();
+                iterator.setCurrentIndex(index);
+                iterator.add();
+                data[index] = element;
+                size++;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -188,11 +262,11 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
         for (E e :
                 elements) {
             if (add(e)) {
-                count++;
+                count++; // Count the change if element was added to the set.
             }
         }
         if (count > 0) {
-            return true;
+            return true; // If there was change, return true else false.
         }
         return false;
     }
@@ -209,11 +283,13 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
     }
 
     /**
-     * @param element @return true if this set contains the specified element
+     * @param element Element to be search for
+     * @return true if this set contains the specified element
      */
     @Override
     public boolean contains(Object element) {
-        if (data[driver((E) element)] == element) {
+        int i = driver((E) element);
+        if (i < size && data[i].equals(element)) {
             return true;
         }
         return false;
@@ -236,16 +312,25 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
      */
     @Override
     public Iterator iterator() {
-        return null;
+        return new CustomIterator();
     }
 
     /**
      * Removes the specified element from this set if it is present.
      *
-     * @param element@return true if this set contained the specified element
+     * @param element element to be removed
+     * @return true if this set contained the specified element
      */
     @Override
     public boolean remove(Object element) {
+        Iterator iterator = this.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(element)) {
+                iterator.remove();
+                size--;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -262,7 +347,7 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
      * (ascending) order.
      */
     @Override
-    public E[] toArray() {
+    public Object[] toArray() {
         return data;
     }
 
@@ -270,10 +355,19 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
      * Removes from this set all of its elements that are contained in the
      * specified collection.
      *
-     * @param elements@return true if this set changed as a result of the call
+     * @param elements @return true if this set changed as a result of the call
      */
     @Override
-    public boolean removeAll(Collection elements) {
+    public boolean removeAll(Collection<?> elements) {
+        int count = 0;
+        for (Object e :
+                elements) {
+            if (remove(e))
+                count++;
+            
+        }
+        if (count > 0)
+            return true;
         return false;
     }
 
@@ -282,18 +376,112 @@ public class BinarySearchSet<E> implements Iterable<E>, SortedSet<E> {
      *                        collection
      */
     @Override
-    public boolean containsAll(Collection elements) {
-        for (int i = 0; i < elements.size(); i++) {
-            if (data[driver((E) elements[i])] == elements[i]) {
-
-            }
-
+    public boolean containsAll(Collection<?> elements) {
+        int count = 0;
+        for (Object e :
+                elements) {
+            if (contains(e))
+                count++;
         }
+
+        if (count == elements.size())
+            return true;
         return false;
     }
 
+    /**
+     * The type Custom iterator.
+     */
+    class CustomIterator implements Iterator<E> {
+
+        /**
+         * The Current index.
+         */
+        int currentIndex;
+
+        /**
+         * Instantiates a new Custom iterator.
+         */
+        public CustomIterator() {
+            currentIndex = 0;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public E next() {
+            if (hasNext()) {
+                E result = data[currentIndex];
+                currentIndex++;
+                return result;
+            }
+            else {
+                throw new NoSuchElementException("Index is out of range.");
+            }
+        }
+
+        /**
+         * Removes from the underlying collection the last element returned
+         * by this iterator (optional operation).  This method can be called
+         * only once per call to {@link #next}.
+         * <p>
+         * The behavior of an iterator is unspecified if the underlying collection
+         * is modified while the iteration is in progress in any way other than by
+         * calling this method, unless an overriding class has specified a
+         * concurrent modification policy.
+         * <p>
+         * The behavior of an iterator is unspecified if this method is called
+         * after a call to the {@link #forEachRemaining forEachRemaining} method.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this iterator
+         * @throws IllegalStateException         if the {@code next} method has not
+         *                                       yet been called, or the {@code remove} method has already
+         *                                       been called after the last call to the {@code next}
+         *                                       method
+         * @implSpec The default implementation throws an instance of
+         * {@link UnsupportedOperationException} and performs no other action.
+         */
+        @Override
+        public void remove() {
+            for (int i = currentIndex - 1; i < size; i++) {
+                data[i] = data[i+1];
+            }
+        }
+
+        public void add() {
+            for (int i = size - 1; i >= currentIndex; i--) {
+                data[i+1] = data[i];
+            }
+        }
+
+        public void setCurrentIndex(int index) {
+            currentIndex = index;
+        }
+    }
 
 
+    /**
+     * The type Natural comparator.
+     *
+     * @param <T> the type parameter
+     */
     class NaturalComparator<T extends Comparable<T>>  implements Comparator<T> {
         @Override
         public int compare(T a, T b){
--- src/assignment03/BinarySearchSetTest.java
+++ src/assignment03/BinarySearchSetTest.java
@@ -1,35 +1,206 @@
 package assignment03;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
-import static org.junit.Assert.*;
-
+/**
+ * The type Binary search set test.
+ */
 public class BinarySearchSetTest/* extends BinarySearchSet*/ {
 
+    /**
+     * The Arr 1.
+     */
     BinarySearchSet<Integer> arr1 = new BinarySearchSet<>();
 
+    /**
+     * Sets up.
+     *
+     * @throws Exception the exception
+     */
     @Before
     public void setUp() throws Exception {
+        arr1.add(1);
+        arr1.add(3);
 
     }
 
+    /**
+     * Tear down.
+     *
+     * @throws Exception the exception
+     */
     @After
     public void tearDown() throws Exception {
     }
 
+    /**
+     * Addition test.
+     */
     @Test
-    public void test1() {
-        Random rand = new Random();
-        int max = 20;
-        int min = -20;
-        for (int i = 0; i < 1000; i++) {
-            arr1.add(rand.nextInt(max - min) + min);
-            System.out.println(arr1.getSize());
+    public void additionTest() {
+        // Test Add
+        Assert.assertTrue(arr1.add(2));
+        System.out.println(Arrays.toString(arr1.toArray()));
+        Assert.assertTrue(arr1.add(5));
+        System.out.println(Arrays.toString(arr1.toArray()));
+        Assert.assertTrue(arr1.add(-1));
+        System.out.println(Arrays.toString(arr1.toArray()));
+        Assert.assertFalse(arr1.add(1));
+        System.out.println(Arrays.toString(arr1.toArray()));
+        Assert.assertFalse(arr1.add(3));
+        System.out.println(Arrays.toString(arr1.toArray()));
+        Assert.assertEquals(5, arr1.size());
+        Object[] arr = new Integer[] {-1,1,2,3,5};
+        if (arr.length == arr1.size()) {
+            for (int i = 0; i < arr.length; i++) {
+                if (!Integer.valueOf((Integer) arr1.toArray()[i]).equals(Integer.valueOf((Integer) arr[i]))) {
+                    Assert.assertEquals(1, 0);
+                }
+            }
+        } else {
+            Assert.assertEquals(0,1);
         }
+
+        // Test Add All
+        ArrayList<Integer> set1 = new ArrayList<>();
+        set1.add(-1);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(5);
+
+        Assert.assertFalse(arr1.addAll(set1));
+
+        set1.add(-11);
+        set1.add(12);
+        set1.add(22);
+        set1.add(23);
+        set1.add(53);
+
+        Assert.assertTrue(arr1.addAll(set1));
+        Assert.assertEquals(10, arr1.size());
+
+        arr = new Integer[] {-11, -1, 1, 2, 3, 5, 12, 22, 23, 53};
+        if (arr.length == arr1.size()) {
+            for (int i = 0; i < arr.length; i++) {
+                if (!Integer.valueOf((Integer) arr1.toArray()[i]).equals(Integer.valueOf((Integer) arr[i]))) {
+                    Assert.assertEquals(1, 0);
+                }
+            }
+        } else {
+            Assert.assertEquals(0,1);
+        }
+
+    }
+
+    /**
+     * Contains test.
+     */
+    @Test
+    public void containsTest() {
+        // Test Contains
+        Assert.assertTrue(arr1.contains(1));
+        Assert.assertFalse(arr1.contains(5));
+
+        // Test Contains All
+        ArrayList<Integer> set1 = new ArrayList<>();
+        set1.add(-1);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(5);
+        set1.add(-11);
+        set1.add(12);
+        set1.add(22);
+        set1.add(23);
+        set1.add(53);
+
+        arr1.addAll(set1);
+
+        Object[] arr = new Integer[] {-11, -1, 1, 2, 3, 5, 12, 22, 23, 53};
+
+        Assert.assertTrue(arr1.containsAll(set1));
+        set1.add(554);
+        Assert.assertFalse(arr1.containsAll(set1));
+
+
+    }
+
+    /**
+     * Removes test.
+     */
+    @Test
+    public void removesTest() {
+        // Test Remove
+        Assert.assertTrue(arr1.remove(1));
+        Assert.assertEquals(1, arr1.size());
+        Assert.assertFalse(arr1.remove(5));
+
+
+        // Test Remove All
+        ArrayList<Integer> set1 = new ArrayList<>();
+        set1.add(-1);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(5);
+        set1.add(-11);
+        set1.add(12);
+        set1.add(22);
+        set1.add(23);
+        set1.add(53);
+
+        Assert.assertTrue(arr1.removeAll(set1));
+        Assert.assertEquals(0, arr1.size());
+    }
+
+    /**
+     * Generic test.
+     */
+    @Test
+    public void genericTest() {
+        // Test Comparator
+        Assert.assertEquals(null, arr1.comparator());
+
+        // Test Size
+        Assert.assertEquals(2, arr1.size());
+
+        // Test First
+        Assert.assertEquals((Integer) 1, arr1.first());
+
+        // Test Last
+        Assert.assertEquals((Integer) 3, arr1.last());
+
+        // Test Clear and isEmpty
+        arr1.clear();
+        Assert.assertTrue(arr1.isEmpty());
+
+        // Test toArray
+        Random random = new Random();
+        int min = -500;
+        int max = 500;
+        for (int i = 0; i < 10000; i++) {
+            arr1.add((Integer) random.nextInt(max - min) + min);
+            System.out.println(Arrays.toString(arr1.toArray()));
+        }
+
+        Object[] arr = arr1.toArray();
+/*
+        for (int i = 0; i < arr1.size() - 1; i++) {
+            if (Integer.valueOf((Integer) arr[i]) > (Integer.valueOf((Integer) arr[i + 1]))) {
+                Assert.assertEquals(0 , 1);
+            }
+        }*/
+
+
+
 
     }
 
